43344396bdd846c6d4b0aa9b5df2c687cdc81aaf
Brian Marete <bmarete10@gmail.com>
2019-04-24 14:58:05 +0300

    Initial structure

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  'use strict';
+  $('.navbar-toggle, nav').click(function () {
+    $('.navbar-toggle').toggleClass('navbar-on');
+    $('nav').fadeToggle();
+    // $('nav').removeClass('nav-hide');
+  });
+});
